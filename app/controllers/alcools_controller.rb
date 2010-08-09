2966cd10668c1753035111de003e6ed3ab4f1169
Belisker <lrodrigues@hypios.com>
2010-08-09 16:00:25 +0200

    commit

---
+++ app/controllers/alcools_controller.rb
@@ -0,0 +1,36 @@
+class AlcoolsController < ApplicationController
+
+  def index
+    @alcools = Alcool.all
+  end
+
+  def new
+    @alcool = Alcool.new()
+  end
+  
+  def create
+    @alcool = Alcool.create(params[:alcool])
+    if @alcool.save
+      redirect_to alcools_path
+    else
+      render "new"
+    end
+  end
+  
+  def decrease(n)
+    
+  end
+  
+  def show
+    @alcool = Alcool.find(params[:id])
+  end
+  
+  def increase(n)
+    
+  end
+  
+  def script
+    
+  end
+
+end
